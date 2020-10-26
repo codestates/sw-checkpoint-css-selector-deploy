62f7fa0a0726be5d86374270c906f56d63fec925
Lee Hoyong <igotoweb@gmail.com>
2020-10-26 12:06:47 +0900

    Updates

---
+++ precache-manifest.d5ecd78c400260e4de3b2c6cb03932f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbe0a2715a6f1861aa0a117ddb5c4f7",
+    "url": "/sw-checkpoint-css-selector-deploy/index.html"
+  },
+  {
+    "revision": "31604376b6ec4e997571",
+    "url": "/sw-checkpoint-css-selector-deploy/static/css/main.ae83b28d.chunk.css"
+  },
+  {
+    "revision": "ebcb144a43baccd8a9f1",
+    "url": "/sw-checkpoint-css-selector-deploy/static/js/2.cfffea70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sw-checkpoint-css-selector-deploy/static/js/2.cfffea70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31604376b6ec4e997571",
+    "url": "/sw-checkpoint-css-selector-deploy/static/js/main.fad70411.chunk.js"
+  },
+  {
+    "revision": "4e0e245bed7e9bd94cfb",
+    "url": "/sw-checkpoint-css-selector-deploy/static/js/runtime-main.61da6142.js"
+  }
+]);
